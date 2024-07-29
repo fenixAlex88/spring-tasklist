afcce5d9b00d79c98c2b97921e1f640b88610313
Alex <fenix_al@tut.by>
2024-07-29 12:44:04 +0300

    add auth service

--- src/main/java/by/fenix/lessons/tasklist/service/impl/AuthServiceImpl.java
+++ src/main/java/by/fenix/lessons/tasklist/service/impl/AuthServiceImpl.java
@@ -1,20 +1,38 @@
 package by.fenix.lessons.tasklist.service.impl;
 
+import by.fenix.lessons.tasklist.domain.user.User;
 import by.fenix.lessons.tasklist.service.AuthService;
+import by.fenix.lessons.tasklist.service.UserService;
 import by.fenix.lessons.tasklist.web.dto.auth.JwtRequest;
 import by.fenix.lessons.tasklist.web.dto.auth.JwtResponse;
+import by.fenix.lessons.tasklist.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+        JwtResponse jwtResponse = new JwtResponse();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword()));
+        User user = userService.getByUserName(loginRequest.getUserName());
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUserName(user.getUserName());
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(user.getId(), user.getUserName(), user.getRoles()));
+        jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(user.getId(), user.getUserName()));
+        return jwtResponse;
     }
 
     @Override
     public JwtResponse refresh(String refreshToken) {
-        return null;
+        return jwtTokenProvider.refreshUsersToken(refreshToken);
     }
 }
