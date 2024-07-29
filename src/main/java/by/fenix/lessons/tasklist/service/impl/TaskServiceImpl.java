a7cc3e516b1f6332736a72f4f752a1baf05dbf31
Alex <fenix_al@tut.by>
2024-07-29 13:08:10 +0300

    add user & task services

--- src/main/java/by/fenix/lessons/tasklist/service/impl/TaskServiceImpl.java
+++ src/main/java/by/fenix/lessons/tasklist/service/impl/TaskServiceImpl.java
@@ -1,36 +1,56 @@
 package by.fenix.lessons.tasklist.service.impl;
 
+import by.fenix.lessons.tasklist.domain.exception.ResourceNotFoundException;
+import by.fenix.lessons.tasklist.domain.task.Status;
 import by.fenix.lessons.tasklist.domain.task.Task;
+import by.fenix.lessons.tasklist.repository.TaskRepository;
+import by.fenix.lessons.tasklist.repository.UserRepository;
 import by.fenix.lessons.tasklist.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
+    private final TaskRepository taskRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public Task getById(long id) {
-        return null;
+        return taskRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task not found"));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Task> getAllByUserId(long id) {
-        return List.of();
+        return taskRepository.findAllByUserId(id);
     }
 
     @Override
+    @Transactional
     public Task update(Task task) {
-        return null;
+        if (task.getStatus() == null)
+            task.setStatus(Status.TODO);
+        taskRepository.update(task);
+        return task;
     }
 
     @Override
-    public Task create(Task task, Long id) {
-        return null;
+    @Transactional
+    public Task create(Task task, Long userId) {
+        task.setStatus(Status.TODO);
+        taskRepository.create(task);
+        taskRepository.assignToUserById(task.getId(), userId);
+        return task;
     }
 
     @Override
+    @Transactional
     public void delete(long id) {
-
+        taskRepository.delete(id);
     }
 }
--- src/main/java/by/fenix/lessons/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/by/fenix/lessons/tasklist/service/impl/UserServiceImpl.java
@@ -1,39 +1,72 @@
 package by.fenix.lessons.tasklist.service.impl;
 
+import by.fenix.lessons.tasklist.domain.exception.ResourceNotFoundException;
+import by.fenix.lessons.tasklist.domain.user.Role;
 import by.fenix.lessons.tasklist.domain.user.User;
+import by.fenix.lessons.tasklist.repository.UserRepository;
 import by.fenix.lessons.tasklist.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
+    @Transactional(readOnly = true)
     public User getById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getByUserName(String userName) {
-        return null;
+        return userRepository.findByUserName(userName)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional
     public User update(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.update(user);
+        return user;
     }
 
     @Override
+    @Transactional
     public User create(User user) {
-        return null;
+        if (userRepository.findByUserName(user.getUserName()).isPresent()) {
+            throw new IllegalStateException("User already exists");
+        }
+        if (!user.getPassword().equals(user.getPasswordConfirmation())) {
+            throw new IllegalStateException("Passwords do not match");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.create(user);
+        Set<Role> roles = Set.of(Role.ROLE_USER);
+        userRepository.insertUserRole(user.getId(), Role.ROLE_USER);
+        user.setRoles(roles);
+        return user;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public boolean isTaskOwner(long userId, long taskId) {
-        return false;
+        return userRepository.isTaskOwner(userId, taskId);
     }
 
     @Override
+    @Transactional
     public void delete(long id) {
-
+        userRepository.delete(id);
     }
 }
